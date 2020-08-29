1fcb01149de698ff430ff9d29498363ad3f265a4
Xephobia <nathanvalletmarseille@gmail.com>
2020-08-29 14:12:54 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 git clone https://github.com/godotengine/godot.git
 powershell Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 powershell Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+refreshenv
 scoop install gcc python scons yasm make
 cd godot
+refreshenv
 scons platform=windows use_mingw=yes use_lto=yes target=release_debug
